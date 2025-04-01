81adf016446aa8529a90370337c47f0bc35eec10
cho <nick2615@nate.com>
2025-04-01 14:51:33 +0900

    이메일체크

--- frontend/src/features/auth/api/useSignupApi.js
+++ frontend/src/features/auth/api/useSignupApi.js
@@ -1,14 +1,40 @@
 
+
+const BASE_URL = "http://localhost:8080";
 //signup에 관련된 api요청코드만 분리해서 모아놓은파일
-export const signupRequst=async(formData)=>{
-    const url="http://localhost:8080/api/auth"
-    const init={
-      method: "POST",
-      headers:{
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData)
-    }
-  const response=await fetch(url, init);
+export const signupRequst = async (formData) => {
+  const url = `${BASE_URL}/api/auth`
+  const init = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData)
+  }
+  const response = await fetch(url, init);
   return response.json();
+}
+
+export const checkEmailExist = async (email) => {
+  const response = await fetch(`${BASE_URL}/api/check-email`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({email})
+  });
+
+  return response.json()
+}
+
+export const checkNickExist = async (nick) => {
+  const response = await fetch(`${BASE_URL}/api/check-nick`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({nick})
+  });
+
+  return response.json()
 }
--- frontend/src/features/auth/model/useSignupForm.js
+++ frontend/src/features/auth/model/useSignupForm.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { validateEmail, validateNickname, validatePassword, validatePasswordConfirm } from "./validate";
+import { checkEmailExist, checkNickExist } from "../api/useSignupApi";
+
 
 //커스텀 훅(회원가입 폼 로직)
 const useSinupForm = () => {
@@ -21,13 +23,50 @@ const useSinupForm = () => {
       default: return '유효한 값이 아니네요'
     }
   }
+  
+  const handleEmailCheck=async(email)=>{
+    try {
+      const result=await checkEmailExist(email);
+      return result;
+    } catch (error) {
+      console.error("이메일 체크 실패", error);
+      setErrors({...errors, email: '서버와 통신 실패'});
+    }
+    return false;
+  }
+
+  const handleNickCheck=async(nick)=>{
+    try {
+      const result=await checkNickExist(nick);
+      return result;
+    } catch (error) {
+      console.error("닉네임 체크 실패", error);
+      setErrors({...errors, nick: '서버와 통신 실패'});
+    }
+    return false;
+  }
 
   //입력을 완료하면 유효성검사를 실시하고 실패시 에러메세지를 저장
-  const handleBlur = (e) => {
+  const handleBlur =async (e) => {
     console.log(e.target)
     const { name, value } = e.target;
     //const error=validate(name, value);
     setErrors({ ...errors, [name]: validate(name, value) });
+    //이메일은 중복체크
+    if(name==='email'){
+      const isEmailExist=await handleEmailCheck(value);
+      console.log("서버가 응답>>>>",isEmailExist);
+      if(isEmailExist){
+        setErrors({...errors, email: '이미 존재하는 이메일'});
+      }
+    }
+    //닉네임도 중복체크
+    if(name==='nick'){
+      const isNickExist=await handleNickCheck(value);
+      if(isNickExist){
+        setErrors({...errors, nick: '이미 존재하는 닉네임'});
+      }
+    }
   }
   const handleChange = (e) => {
     const { name, value } = e.target;
